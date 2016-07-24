6e7d3a03de60f589528d3251faa9e96bc43497af
Jessica Wheeler <jessicasibal@hotmail.com>
2016-07-23 20:11:15 -0500

    Add files via upload

---
+++ analysis/Paper/MasterMakefile_RedEtel.R
@@ -0,0 +1,24 @@
+####################################################################
+### MAKEFILE CHULWALHAR    
+### Bill Kerneckel, Nathan Mowat, Chris Woodard, Jessica Wheeler
+### Created July 23, 2016
+### Updated July 23, 2016
+#####################################################################
+
+#Set working directory
+setwd("https://github.com/wkerneck/CaseStudy2/tree/master/analysis/data")
+
+#File to load and clean the data:
+source("CaseStudy2/analysis/data/1.md")
+
+
+#Analysis files:
+source("CaseStudy2/analysis/data/2.md")
+source("CaseStudy2/analysis/data/3.md")
+source("CaseStudy2/analysis/data/4.md")
+source("CaseStudy2/analysis/data/5.md")
+source("CaseStudy2/analysis/data/6.md")
+source("CaseStudy2/analysis/data/7.md")
+source("CaseStudy2/analysis/data/8.md")
+source("CaseStudy2/analysis/data/9.md")
+source("CaseStudy2/analysis/data/10.md")
