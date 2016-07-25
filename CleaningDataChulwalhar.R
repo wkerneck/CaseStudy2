143ed3512b0073eef6e372c2eafc13799d4be48e
Bill Kerneckel <kerneckel@gmail.com>
2016-07-24 19:23:16 -0500

    Delete CleaningDataChulwalhar.R

--- CleaningDataChulwalhar.R
+++
@@ -1,8 +0,0 @@
-#Cleaning the data
-#Module 1- 1.2
-RedEtelAsIsVector <- c(ImportedAsIsData [72:83,2],ImportedAsIsData [72:83,3],ImportedAsIsData [72:83,4],ImportedAsIsData [72:83,5],ImportedAsIsData [72:83,6],ImportedAsIsData [72:83,7])
-RedEtelPlanVector <- c(ImportedPlanData[72:83,2],ImportedPlanData[72:83,3],ImportedPlanData[72:83,4],ImportedPlanData[72:83,5],ImportedPlanData[72:83,6],ImportedPlanData[72:83,7])
-RedEtelAsIs <- ts(RedEtelAsIsVector, start=c(2008,1), end=c(2013,12), frequency=12)
-RedEtelPlan <- ts(RedEtelPlanVector, start=c(2008,1), end=c(2013,12), frequency=12)
-plot(RedEtelAsIs, col="red", main="RedEtelAsIs")
-plot(RedEtelPlan, col="red", main="RedEtelPlan")
